27e92ca7a43d428733eece92a8053efd7e7e2bd9
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-09-02 17:08:13 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+
 const port = 3000
+//possibilita a utilização de arquibos ejs renderizados pelo express
+app.set('view engine', 'ejs')
+// para alterar a pasta padrão 'views' deve inserir o código:
+//  app.set('views', 'nova/pasta') 
+
 app.all('/', (req, res, next) => {
   console.log(req.url + ' - ' + req.method)
   next()
 })
+
 app.get('/', (req, res) => {
   res.send('Bem vindos turma 6C - GET')})
 app.post('/', (req, res) => {
@@ -13,6 +20,15 @@ app.put('/', (req, res) => {
   res.send('Bem vindos turma 6C - PUT')})
 app.delete('/', (req, res) => {
   res.send('Bem vindos turma 6C - Delete')})
+
+
+app.get('/cursos', (req, res) => {
+  res.render('cursos')
+})
+app.get('/pesquisa', (req, res) => {
+  res.render('pesquisa')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
