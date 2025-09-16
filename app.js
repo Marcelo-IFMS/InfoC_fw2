70881036d7da46eae3fd65a040a90803b12927a7
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-09-16 17:34:37 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
 const app = require('./config/server')
 const port = process.env.port //set port=3000; set <listas as variáveis de Ambiente>
 const boasVindas = require('./modulo')
-//  console.log(`${boasVindas}, aberto na porta: ${port}`)
-app.all('/', (req, res, next) => {
-  console.log(req.url + ' - ' + req.method)
-  next()
-})
-const index = require('./rotas/index')
-index(app);
-const cursos = require('./rotas/cursos')
-cursos(app);
-const pesquisa = require('./rotas/pesquisa')
-pesquisa(app);
-
-
-
-app.post('/', (req, res) => {
-  res.send('Bem vindos turma 6C - Post')
-})
-app.put('/', (req, res) => {
-  res.send('Bem vindos turma 6C - PUT')
-})
-app.delete('/', (req, res) => {
-  res.send('Bem vindos turma 6C - Delete')
-})
-
-
 
 app.listen(port, () => {
   console.log(`${boasVindas}, aberto na porta: ${port}`)
--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const consign = require('consign')
 
 //possibilita a utilização de arquibos ejs renderizados pelo express
 app.set('view engine', 'ejs')
 // para alterar a pasta padrão 'views' deve inserir o código:
-//  app.set('views', 'nova/pasta') 
+// app.set('views', 'nova/pasta') 
 app.use(express.static('public'))
+consign().include('/rotas').into(app)
 
 module.exports = app;
