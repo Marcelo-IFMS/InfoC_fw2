dd14f48ad0ad3df4d833862cfcf44d377de1f5ef
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-10-06 16:33:09 -0400

    Add files via upload

--- rotas/db.js
+++ rotas/db.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
     app.get('/db',  async (req, res) => {
         try {
             await app.dbClient.connect();
-            const users = await app.dbClient.db('sample_mflix').collection('users').find({ email: /gmail.com/ }).toArray();
+            const users = await app.dbClient.db('sample_mflix').collection('users').find({ name: /^A/ }).toArray();
             const size = Object.keys(users).length
             res.json({ msg: `Arquivo db.js executado ${size}`, resultado: users });
             
